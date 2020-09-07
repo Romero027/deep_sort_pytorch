144d94d22c60188f2301f0a345c0a0a6821ab7db
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2020-09-06 21:51:10 -0400

    add download script

---
+++ data/download_MOT16.sh
@@ -0,0 +1,8 @@
+echo "Downloading MOT16-02"
+wget -O MOT16-02.avi https://www.dropbox.com/s/4amlw2uo8mn3ryp/MOT16-02.avi?dl=0
+
+echo "Downloading MOT16-04"
+wget -O MOT16-04.avi https://www.dropbox.com/s/g6yjgcfoef0m9s1/MOT16-04.avi?dl=0
+
+echo "Downloading MOT16-05"
+wget -O MOT16-05.avi https://www.dropbox.com/s/uv6m9yrkjrc5faf/MOT16-05.avi?dl=0
