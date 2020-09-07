c01d2de65b3b6245e73f1f32b7ce88dbee43fc9b
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2020-09-06 20:18:20 -0400

    Tweaks

--- utils/video_resolution.py
+++ utils/video_resolution.py
@@ -21,15 +21,18 @@ resolution_dict = {'2160p': (3840, 2160),
 FPS = 30
 INPUT_SCALE = resolution_dict[input_resolution]
 TARGET_SCALE = resolution_dict[output_resolution]
-print(f"Changing resolution from {INPUT_SCALE} to {TARGET_SCALE}")
+print(f"Changing resolution from {input_resolution}: {INPUT_SCALE} to {output_resolution}: {TARGET_SCALE}")
 
 cap = cv2.VideoCapture(input_video)
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter(output_video, fourcc, FPS, INPUT_SCALE)
 
+idx = 0
 while True:
     ret, frame = cap.read()
+    if idx % 500 == 0:
+        print(f"Processed {idx} frames")
     if ret == True:
         b = cv2.resize(frame, TARGET_SCALE, fx=0, fy=0, interpolation = cv2.INTER_LINEAR)
         b = cv2.resize(b, INPUT_SCALE, fx=0, fy=0, interpolation = cv2.INTER_LINEAR)
